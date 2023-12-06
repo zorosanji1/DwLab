6a2bc97f0f0a243eed3660e608094dca72513573
zorosanji1 <149530957+zorosanji1@users.noreply.github.com>
2023-12-06 20:03:12 +0530

    Delete read_xml_file.py

--- read_xml_file.py
+++
@@ -1,6 +0,0 @@
-import xml.etree.ElementTree as ET
-file_path=r"C:\Users\gokul\Desktop\DW Lab\read_xml_file\data.xml"
-tree=ET.parse(file_path)
-root=tree.getroot()
-for i in root.iter():
-    print(f"Tag: {i.tag}, Text: {i.text}")
